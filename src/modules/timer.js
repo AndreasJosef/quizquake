9fef7156729fdf963708d2e261c14432682511ed
YoViking <joakim.viking@gmail.com>
2025-11-10 14:41:42 +0100

    Timer + bonusfunction done

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,18 +1,53 @@
 // Timer
 
+const TOTAL_TIME = 60;
+
+let secondRemaining = TOTAL_TIME;
+
+let timerId = null;
+
+export function tick() {
+
+    secondRemaining--;
+
+    if (secondRemaining <=0) {
+        clearInterval(timerId);
+        timerId = null
+    }
+}
 
 //when time is up call callback
-export function startTimer(time, callback){
+export function startTimer(){ 
+
+    if (timerId !== null) {
+        return;
+    }
     
+    timerId = setInterval(tick, 1000);
 }
 
 // tar bort timern
-export function reset() {
+export function resetTimer() {
+    clearInterval(timerId);
+
+    timerId = null;
+
+    secondRemaining = TOTAL_TIME;
 
 }
 
 
 
 // privat
+// Turns the page gradually from green to red as time ticking. 
+
+function updateVisuals() {
+
+    const percentage = secondRemaining / TOTAL_TIME;
+
+    const hue = percentage * 120;
+
+    document.body.style.backgroundColor = `hsl(${hue}, 100%. 50%)`;
+}
 
 
