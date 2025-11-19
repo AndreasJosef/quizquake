08bf4457dd44ef402f7c1d2183564e11bb441431
AndreasJosef <hello.andreas@protonmail.com>
2025-11-19 12:21:49 +0100

    refactor: cleaned up questionsAdapter

--- src/modules/questionsAdapter.js
+++ src/modules/questionsAdapter.js
@@ -1,9 +1,7 @@
-
 // ===== fetch JSON =====
 export async function fetchQuestions(category) {
 
   const res = await fetch(`/src/assets/${category}.json`);
-  // const res = await fetch('/src/assets/questions.json');
 
   if (!res.ok) {
     throw new Error("Kunde inte läsa in questions.json");
@@ -14,25 +12,22 @@ export async function fetchQuestions(category) {
   const allQuestions = rawData.quiz.flatMap(item => item.questions)
 
   return getRandomQuestions(allQuestions, 50);
+}
 
- function getRandomQuestions(questions, count){
+
+function getRandomQuestions(questions, count) {
 
   const shuffled = [...questions];
 
   // Fisher-Yates shuffle algoritm (bäst enligt internet)
-  for (let i = shuffled.length - 1; i > 0; i--){
+  for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
 
-    
+
   }
 
   return shuffled.slice(0, Math.min(count, shuffled.length));
 
 
- }
-
-  // return rawData.quiz.flatMap(item => item.questions)
-
 }
-
