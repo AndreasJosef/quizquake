5630d651d51791b866abeac7a70de991d8eb0743
Cristian <cristian.castro@homail.se>
2025-11-21 12:07:39 +0100

    add audioService.js

--- src/modules/timerService.js
+++ src/modules/timerService.js
@@ -38,41 +38,6 @@ export function resetTimer() {
 }
 
 
-// Put tick-tack sound to timer
-
-const tickSound = new Audio(); //tick.mp3 ligger i assets
-const tackSound = new Audio(); //tack.mp3 ligger i assets
-
-
-function tickTack(){
-
-    let progress = 1 - (secondsRemaining / TOTAL_TIME);
-
-    let currentVolume = Math.max(0.1, progress);     
-
-    tickSound.volume = currentVolume; 
-    tackSound.volume = currentVolume;
-
-    if (secondsRemaining % 2 === 1) {
-
-        tickSound.currentTime = 0;
-        tickSound.play().catch(e => console.log(e));
-    }else {
-        tackSound.currentTime = 0;
-        tackSound.play().catch(e => console.log(e));
-    }
-
-    if (secondsRemaining === 0) { 
-        clearInterval(timerId);
-        timerId = null;
-    }
-
-}
- 
-// Turns the page gradually from green to red as time ticking. 
-
-
-
 
 
 
