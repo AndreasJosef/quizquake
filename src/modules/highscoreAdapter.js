f4b29e5a943064fb5659189c972086bb98049157
AndreasJosef <hello.andreas@protonmail.com>
2025-11-24 09:05:08 +0100

    refactor: Clean up highscoreAdapter

--- src/modules/highscoreAdapter.js
+++ src/modules/highscoreAdapter.js
@@ -2,6 +2,7 @@ import { bus } from "../core/eventBus";
 
 const STORAGE_KEY = "quiz_highscores";
 
+// saves a top 10 sorted list of highscore objects to local storage
 export function saveHighscore(score, name) {
 
   const highscores = getHighscores(STORAGE_KEY);
@@ -16,27 +17,28 @@ export function saveHighscore(score, name) {
 
   if (scoreExists) return 
 
-  if (highscores.includes(entry.score)) return
-
   highscores.push(entry);
 
   // Sort from highest to lowest
   highscores.sort((a, b) => b.score - a.score);
 
+  // only save top ten
   const topTen = highscores.slice(0, 10);
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(topTen));
 
   bus.emit('storage', topTen);
 
-  console.log("Highscore saved!", entry.player, entry.score);
+  console.log("Highscore saved:", entry.player, entry.score);
 }
 
+// returns a sorted array of highscore objets from local storage
 export function getHighscores() {
   const data = localStorage.getItem(STORAGE_KEY);
   return data ? JSON.parse(data) : [];
 }
 
+// deletes all highscores from local storage
 export function clearStorage() {
   localStorage.setItem(STORAGE_KEY, [])
   console.log("Cleared Highscores!", localStorage.getItem(STORAGE_KEY));
